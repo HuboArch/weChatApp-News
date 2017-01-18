ae0a4a640fbb13cb1a6ec84b498a406ed05343b7
elegantspirit <hkissbigapple@163.com>
2017-01-18 19:43:39 +0800

    添加事件

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,13 +1,14 @@
 var postsData = require('../../data/posts-data');
 
 Page({
-  data:{
-    
-  },
-  onReady:function(){
-    // 生命周期函数--监听页面初次渲染完成
-      this.setData({
-          post_key: postsData.postList
-      });
-  }
+    data: {},
+    fun: function () {
+        console.log('hello');
+    },
+    onReady: function () {
+        // 生命周期函数--监听页面初次渲染完成
+        this.setData({
+            post_key: postsData.postList
+        });
+    }
 });
