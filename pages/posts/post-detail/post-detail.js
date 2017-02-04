6b2e91b9a26fcb6ec3795dc0d3ef2e8054855e70
elegantspirit <hkissbigapple@163.com>
2017-02-04 18:31:02 +0800

    分享功能实现

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -6,8 +6,39 @@ Page({
         var postsData = postData.postList[postId];
 
         this.data.post = postsData;
+        this.data.currentPostId = postId;
+
+        var postsCollected = wx.getStorageSync('posts-collected');
+
+        if (postsCollected) {
+            postsCollected = postsCollected[postId];
+
+            this.setData({
+                collected: postsCollected
+            });
+        } else {
+            postsCollected = {};
+            postsCollected[postId] = false;
+
+            wx.setStorage('posts-collected', postsCollected)
+        }
     },
+
     onReady: function () {
         this.setData(this.data);
+    },
+
+    onCollectionTap: function (event) {
+        var postsCollected = wx.getStorageSync('posts-collected');
+        var postCollected = postsCollected[this.data.currentPostId];
+
+        postCollected = !postCollected;
+
+        //小程序是单向数据绑定
+        postsCollected[this.data.currentPostId] = postCollected;
+        wx.setStorageSync('posts-collected', postsCollected);
+        this.setData({
+            collected: postCollected
+        });
     }
 });
